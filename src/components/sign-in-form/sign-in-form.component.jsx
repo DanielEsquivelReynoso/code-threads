c02aa4603f98dd6050a72c2ff4231eedb92b3eb6
DanielEsquivelReynoso <daniel.esquivelreynoso@gmail.com>
2022-11-07 14:52:05 -0800

    completed final details of sign-in form and style

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -39,11 +39,17 @@ const SignInForm = () => {
       console.log(response);
       resetFormFields();
     } catch (error) {
-      if (error.code === 'auth/wrong-password') {
-        alert('Incorrect password for email')
+      switch(error.code) {
+        case 'auth/wrong-password':
+          alert('incorrect password for email');
+          break;
+        case 'auth/user-not-found':
+          alert('no user associated with this email');
+          break;
+        default:
+          console.log(error);
       }
-      console.log('error for sign in: ', error)
-    };
+    }
   };
 
   const handleChange = (event) => {
@@ -60,7 +66,7 @@ const SignInForm = () => {
         <FormInput label='Password' type='password' required onChange={handleChange} name='password' value={password}/>
         <div className='buttons-container'>
           <Button buttonType='default' type='submit'>Sign In</Button>
-          <Button buttonType='google 'onClick={signInWithGoogle}>Google Sign In</Button>
+          <Button buttonType='google' type='button' onClick={signInWithGoogle}>Google Sign In</Button>
         </div>
       </form>
     </div>
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,25 +1,12 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils.js'
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component.jsx';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component.jsx';
 import './authentication.styles.scss';
 
 const Authentication = () => {
-
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user)
-  }
-
-
   return (
-    <div>
-      <h1>
-        Sign In Page
-      </h1>
-      <div className='forms'>
-        <SignInForm />
-        <SignUpForm />
-      </div>
+    <div className='authentication-container'>
+      <SignInForm />
+      <SignUpForm />
     </div>
   )
 }
