e17637257c5258ac3dc49ee18d6313e0d6392999
DanielEsquivelReynoso <daniel.esquivelreynoso@gmail.com>
2023-02-19 12:22:53 -0800

    isolating authentication completed

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,21 +1,23 @@
 import { createContext, useState, useEffect } from 'react';
-import { onAuthStateChangedListener, signOutUser } from '../utils/firebase/firebase.utils';
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
 
 // actual value you want to access elsewhere in the application
 export const UserContext = createContext({
-  currentUser: null,
-  setCurrentUser: () => null
+  setCurrentUser: () => null,
+  currentUser: null
 })
 
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser ] = useState(null);
   const value = { currentUser, setCurrentUser };
-  signOutUser();
+
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
-      console.log(user);
-    })
-
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
     return unsubscribe;
   }, [])
 
