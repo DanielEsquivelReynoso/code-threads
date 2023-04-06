86b9d1b22a85655951ab865d940757d71c7a413c
DanielEsquivelReynoso <daniel.esquivelreynoso@gmail.com>
2023-04-06 16:53:17 -0700

    cart component progress

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,9 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component.jsx';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component.jsx';
+
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context.jsx';
 
@@ -39,7 +42,9 @@ const Navigation = () => {
               </Link>
             )
           }
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,16 +3,17 @@
 // What do we do with shop data? --> It will likely live in Firebase DB. We can start setup the storage with React Context
 
 import { useContext } from 'react';
-import { ProductsContext } from '../../contexts/products.context';
+import { ProductsContext } from '../../contexts/products.context.jsx';
+import ProductCard  from '../../components/product-card/product-card.component.jsx';
+
+import './shop.styles.scss';
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
   return (
-    <div>
-      {products.map(({id, name}) => (
-        <div key= {id}>
-          <h1>{name}</h1>
-        </div>
+    <div className='products-container'>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product}/>
       ))}
     </div>
   )
