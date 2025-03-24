303637dddd915d08aa139eba35b1f2c6a6fb05f4
Daniel Esquivel-Reynoso <daniel.esquivelreynoso@gmail.com>
2025-03-24 08:12:51 -0700

    updated userContext

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -7,6 +7,7 @@ export const UserContext = createContext({
   currentUser: null
 })
 
+// 
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser ] = useState(null);
   const value = { currentUser, setCurrentUser };
