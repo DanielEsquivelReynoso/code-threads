f0365fb6dbc295a1972249a406fee0d0a76e4feb
Daniel Esquivel-Reynoso <daniel.esquivelreynoso@gmail.com>
2025-07-07 16:08:17 -0700

    created checkout cart total functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -49,13 +49,15 @@ export const CartContext = createContext({
   addItemToCart: () => {},
   removeItemFromCart: () => {},
   clearItemFromCart: () => {},
-  cartCount: 0
+  cartCount: 0,
+  cartTotal: 0
 });
 
 export const CartProvider = ({children}) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0)
 
   // changes the cart items amount with the dependency array of [cartItems]
   useEffect(() => {
@@ -64,6 +66,14 @@ export const CartProvider = ({children}) => {
       0
     );
     setCartCount(newCartCount);
+  }, [cartItems]);
+
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    );
+    setCartTotal(newCartTotal);
   }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
@@ -78,7 +88,16 @@ export const CartProvider = ({children}) => {
     setCartItems(clearCartItem(cartItems, cartItemToClear));
   };
 
-  const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemFromCart, clearItemFromCart};
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    cartCount,
+    removeItemFromCart,
+    clearItemFromCart,
+    cartTotal
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 };
